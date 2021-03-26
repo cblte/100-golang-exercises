4f40c93c3f9edb2c000cb450f814c8c3270f05bf
cblte <mail@cbrueggenolte.de>
2021-03-26 17:57:40 +0100

    Update and Rename of exercise-001.go to exercise001.go

--- 001/exercise001.go
+++ 001/exercise001.go
@@ -1,5 +1,11 @@
 package main
 
+/*
+Exercise 001:
+
+Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included).  The numbers obtained should be printed in a comma-separated sequence on a single line.
+*/
+
 import (
 	"fmt"
 	"strconv"
