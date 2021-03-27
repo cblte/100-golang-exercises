603e4a3d7bd4ba2ba77de46d7d10999aadfc0b87
cblte <cbrueggenolte@me.com>
2021-03-27 18:03:07 +0100

    Update exercise001_test.go

--- 001/exercise001_test.go
+++ 001/exercise001_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,9 +8,6 @@ func TestEx001(t *testing.T) {
 
 	want := "112,119,126,133,147,154,161,168,182,189,196"
 	got := Ex001(100, 200)
-
-	fmt.Printf("%T - %v\n", want, want)
-	fmt.Printf("%T - %v\n", got, got)
 
 	if got != want {
 		t.Errorf("Ex001() = %v, want %v", got, want)
