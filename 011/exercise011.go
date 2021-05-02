57aac3a71a797dc9f80c4d23e08fe96b9a748ded
cblte <cbrueggenolte@me.com>
2021-05-02 18:45:45 +0200

    Add exercise 011

---
+++ 011/exercise011.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Ex011 takes comma seperated string with 4 digits binary numbers
+// returns a command seperatad string which the numbers which can
+// be deviced by 5
+func Ex011(input string) string {
+
+	// split string
+	numbers := strings.Split(input, ",")
+	result := []string{}
+
+	// loop over numbers
+	for _, v := range numbers {
+		// convert
+		converted, _ := strconv.ParseInt(v, 2, 64)
+		// if mod 5 == 0
+		if converted%5 == 0 {
+			// convert back to binary and add to result slice
+			// s := strconv.FormatInt(converted, 2)
+			// result = append(result, s)
+
+			// just add v to the result
+			result = append(result, v)
+		}
+
+	}
+	// join output back together
+	output := strings.Join(result, ",")
+
+	return output
+}
---
+++ 011/exercise011_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Write a program which accepts a sequence of comma separated 4 digit binary
+// numbers as its input and then check whether they are divisible by 5 or not.
+// The numbers that are divisible by 5 are to be printed in a comma
+// separated sequence.
+
+func TestEx011(t *testing.T) {
+
+	input := "0100,0011,1010,1001"
+	want := "1010"
+
+	got := Ex011(input)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Ex010() = %v, want %v", got, want)
+	}
+
+}
